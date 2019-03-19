b0ca9a7e8f08e251a4a1eaa8d648fd0aad10fe68
Tiago Guizelini <tiago.guizelini@7comm.com.br>
2019-03-19 13:11:36 -0300

    tira a rota com /id/ para busca participante por ID

--- app/routes/Participante.js
+++ app/routes/Participante.js
@@ -2,7 +2,7 @@ module.exports = app => {
   const controller = app.controllers.Participante()
 
   app.get('/participantes', controller.list)
-  app.get('/participantes/id/:id', controller.find)
+  app.get('/participantes/:id', controller.find)
   app.post('/participantes', controller.save)
   app.put('/participantes', controller.update)
   app.delete('/participantes/:id', controller.deleteById)
