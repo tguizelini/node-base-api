33227eda6a8ee09e025d87c6205cff648080e679
Tiago <tiguizelini@gmail.com>
2019-03-21 12:48:36 -0300

    porta dinamica

--- config/middlewares.js
+++ config/middlewares.js
@@ -2,7 +2,7 @@ const bodyParser = require('body-parser')
 const cors = require('cors')
 
 module.exports = app => {
-  app.set('port', 3000)
+  app.set('port', process.env.PORT || 3000)
   app.use(cors())
   //app.use(express.static('../public'))
   app.use(bodyParser.urlencoded({ extended: false }))
