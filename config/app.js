334359ad0a4df691d88eefc5e43e0b6055dfe20b
Tiago Guizelini <tiago.guizelini@7comm.com.br>
2019-03-25 16:36:02 -0300

    limpa dados tabela

--- config/app.js
+++ config/app.js
@@ -26,7 +26,7 @@ module.exports = () => {
     .catch(err => console.error('Database: Unable to connect to the database:', err))
 
   app.sequelize
-    .sync({ force: false }) 
+    .sync({ force: true }) 
     .then(() => console.log(`DATABASE: Tables created!`))
 
   return app
